5904919813cb76ce90795b80b3f938392a42cdc1
XanderUZZZER <xanderuzzzer@gmail.com>
2019-02-22 18:06:51 +0200

    Add project files.

---
+++ DependencyInjection/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using DependencyInjection.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyInjection.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IRepository Repository { get; set; } = new MemoryRepository();
+
+        public ViewResult Index() => View(Repository.Products);
+    }
+}
